389d5aa11a549ee4792be4e287d1133f58954f8c
Iurii Kyian <iurii.kyian@insights.md>
2019-04-26 17:04:09 +0200

    initial models usage in template

--- spike01models/notes/urls.py
+++ spike01models/notes/urls.py
@@ -4,4 +4,5 @@ from notes import views
 urlpatterns = [
     path('', views.index),
     path('edit/', views.edit),
+    path('edit/<int:id>', views.edit),
 ]
--- spike01models/notes/views.py
+++ spike01models/notes/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from notes.models import Note
 
 # Create your views here.
 
@@ -7,7 +10,25 @@ def index(request):
         "notes" : []
     })
 
-def edit(request):
-    return render(request, 'notes/edit.html', {
-        "note" : None
-    })
+def edit(request, id=None):
+    if request.method == 'GET':
+        n = {}
+        if id:
+            n = Note.objects.get(pk=id)
+        return render(request, 'notes/edit.html', {
+            "note" : n
+        })
+    elif request.method == 'POST':
+        print('POST')
+        print(request.POST)
+        pk = request.POST['pk']
+        text = request.POST['text']
+        if pk:
+            n = Note.objects.get(pk=pk)
+            #TODO: error handling
+        else:
+            n = Note(text=text)
+        n.text = text
+        n.save()
+        return HttpResponseRedirect('/')
+            
